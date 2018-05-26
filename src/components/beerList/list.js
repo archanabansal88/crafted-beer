da60b2191f991ad1b5a90b304bddeb523bebba1c
Archana Bansal <archanamittal0388@gmail.com>
2018-05-26 21:42:50 +0530

    Feat: displaying alcohol content in list of beer

--- src/components/beerList/list.js
+++ src/components/beerList/list.js
@@ -2,15 +2,17 @@ import React from 'react'
 
 const List = ({data}) => {
   return (
-    <div className='container'>
+    <div className='panel-block'>
       <ul className='columns is-multiline'>
         {data.map((value, index) => {
+          const {name, style, abv, ounces} = value
           return <li key={index} className='column is-4'>
             <div className='card'>
               <div className='card-content'>
-                <div className='title is-5'>{value.name}</div>
-                <div className='subtitle'>{value.style}</div>
-                <div>{value.ounces} oz</div>
+                <div className='title is-5'>{name}</div>
+                <div className='subtitle'>{style}</div>
+                {abv && <div>Alcohol Content- {abv} %</div>}
+                <div>{ounces} oz</div>
               </div>
               <footer className='card-footer'>
                 <a href='#' className='card-footer-item'>Add to Cart</a>
